d9767bd382950cbf9be76dffeb08c88acd1fc942
Blaze Sanders Linode <blazes@mfc.us>
2023-08-09 18:43:19 +0000

    Hardcode minus 5 hours offset, fix employee check out label, and remove extra spaces

--- Main.py
+++ Main.py
@@ -16,8 +16,8 @@ __doc__        = "Generate a Progressive Web App GUI to log employee check-in an
 # pylint: disable=global-statement
 
 # Standard Python libraries
-from datetime import datetime, time # Manipulate calendar dates & time objects https://docs.python.org/3/library/datetime.html
-import re                           # Regular Expression matching operations https://docs.python.org/3/library/re.html
+from datetime import datetime, time, timedelta 	# Manipulate calendar dates & time objects https://docs.python.org/3/library/datetime.html
+import re                           		# Regular Expression matching operations https://docs.python.org/3/library/re.html
 
 # Internally developed modules
 import GlobalConstants as GC              # Global constants used across MainHouse.py, HouseDatabase.py, and PageKiteAPI.py
@@ -42,14 +42,14 @@ validEmployeeID = ''
 def build_svg() -> str:
     """ Create an 800 x 800 pixel clock in HTML / SVG
         https://de.m.wikipedia.org/wiki/Datei:Station_Clock.svg
-        
+
     Args:
         NONE
 
     Returns:
         str: Valid HTML to create an analog clock
     """
-    now = datetime.now()
+    now = datetime.now() - timedelta(hours=5)
     return f'''
         <svg width="800" height="800" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <circle cx="400" cy="400" r="200" fill="#fff"/>
@@ -83,27 +83,28 @@ def build_svg() -> str:
     '''
 
 def clock_x(direction: int, sanitizedID: str):
-    """ Perform database insert 
+    """ Perform database insert
 
     Args:
         direction (CONSTANT int):Define function as clock IN or clock OUT method
         sanitizedID (str): Global sanitized number entered into Number text  box
     """
-    
+
     if invalidIdLabel.visible == False and len(sanitizedID) == GC.VALID_EMPLOYEE_ID_LENGTH:
         if direction == GC.CLOCK_IN:
             clockedInLabel.set_text(f'{sanitizedID} - REGISTRO EN (CLOCKED IN)')
+            clockedInLabel.visible = True
             db.insert_check_in_table(sanitizedID)
         elif direction == GC.CLOCK_OUT:
             clockedOutLabel.set_text(f'{sanitizedID} - RELOJ DE SALIDA (CLOCK OUT)')
+            clockedOutLabel.visible = True
             db.insert_check_out_table(sanitizedID)
-        
-        clockedInLabel.visible = True
+
     else:
        tryAgainLabel.visible = True
-    
-    inputBox.set_value(None)                          # Clear user input box after set_value('') doesn't work :)  
-                         
+
+    inputBox.set_value(None)                          # Clear user input box. Note set_value('') doesn't work :)
+
 
 def sanitize_employee_id(inputText: str) -> str:
     """ Convert all bad user input to valid ouput and update GUI label visibility to control datatbase writes
@@ -121,7 +122,7 @@ def sanitize_employee_id(inputText: str) -> str:
         return 'ID DE EMPLEADO NO VÁLIDO (INVALID EMPLOYEE ID)'
     else:
        invalidIdLabel.visible = False
-    
+
     if inputText == '0':
         sanitizedID = ''
     else:
@@ -150,11 +151,11 @@ def generate_report(db):
 
 
 if __name__ in {"__main__", "__mp_main__"}:
-    
+
     # https://computingforgeeks.com/how-to-install-python-latest-debian/
-    
+
     config = dotenv_values()
-    
+
     db = Database()
     #command = ['python3', 'pagekite.py', f'{GC.LOCAL_HOST_PORT_FOR_GUI}', 'timetracker.pagekite.me']
 
@@ -163,16 +164,16 @@ if __name__ in {"__main__", "__mp_main__"}:
     ui.timer(GC.LABEL_UPDATE_TIME, lambda: tryAgainLabel.set_visibility(False))
     ui.timer(GC.DATABASE_DAILY_REPORT_UPDATE_TIME, lambda: generate_report(db))
     ui.timer(GC.CLOCK_UPDATE_TIME, lambda: clock.set_content(build_svg()))
-    
+
     invalidIdLabel = ui.label('ID DE EMPLEADO NO VÁLIDO (INVALID EMPLOYEE ID)').style("color: red; font-size: 200%; font-weight: 300").classes("self-center")
     invalidIdLabel.visible = False
-    
+
     inputBox = ui.number(label='Ingrese su identificación de empleado', placeholder='Enter your Employee ID', value='', \
                         format='%i', \
                         step='1000', \
                         on_change=lambda e: invalidIdLabel.set_text(sanitize_employee_id(e.value)), \
                         validation={'ID DE EMPLEADO NO VÁLIDO (INVALID EMPLOYEE ID)': lambda value: int(sanitizedID) <= 9999})
-    
+
     inputBox.classes("self-center").style("padding: 40px 0px; width: 800px; font-size: 30px;").props('clearable')
 
     # Invisible character https://invisibletext.com/#google_vignette
@@ -180,14 +181,14 @@ if __name__ in {"__main__", "__mp_main__"}:
         with ui.button(on_click=lambda e: clock_x(GC.CLOCK_IN, sanitizedID), color="green").classes("relative  h-32 w-96"):
             ui.label('RELOJ EN (CLOCK IN) ㅤ').style('font-size: 150%; font-weight: 300')
             ui.icon('login')
-        
+
         with ui.button(on_click=lambda e: clock_x(GC.CLOCK_OUT, sanitizedID), color="red").classes("relative  h-32 w-96"):
             ui.label('RELOJ DE SALIDA (CLOCK OUT) ㅤ').style("font-size: 150%; font-weight: 300")
             ui.icon('logout')
-    
+
     clockedInLabel = ui.label(f'{validEmployeeID} - REGISTRO EN (CLOCKED IN)').style("color: green; font-size: 400%; font-weight: 300").classes("self-center")
-    clockedOutLabel = ui.label(f'{validEmployeeID} - FINALIZADO (CLOCKED OUT)').style("color: red; font-size: 400%; font-weight: 300").classes("self-center")    
+    clockedOutLabel = ui.label(f'{validEmployeeID} - FINALIZADO (CLOCKED OUT)').style("color: red; font-size: 400%; font-weight: 300").classes("self-center")
     tryAgainLabel = ui.label('INTENTAR OTRA VEZ (TRY AGAIN)').style("color: red; font-size: 400%; font-weight: 300").classes("self-center")
-    
+
     ui.run(native=GC.RUN_ON_NATIVE_OS, port=GC.LOCAL_HOST_PORT_FOR_GUI)
-    
+
