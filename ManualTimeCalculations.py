01fb09bff72ff75c683f31e2c12623bbd8d150e7
Blaze Sanders <blazes@mfc.us>
2023-09-06 11:43:24 -0500

    Working manual csv creation with weekly schedule threading

--- ManualTimeCalculations.py
+++ ManualTimeCalculations.py
@@ -9,8 +9,30 @@ import pytz 					                # World Timezone Definitions  https://pypi.org/
 import os
 import csv
 
+import schedule
+import threading            # https://schedule.readthedocs.io/en/stable/background-execution.html
+
 # Internal modules
 import GlobalConstants as GC
+
+def get_date_time() -> datetime:
+    """ Get date and time in Marianna, FL timezone, independent of location on server running code
+
+    Returns:
+        Datetime: 
+    """
+    tz = pytz.timezone('America/Chicago')
+    zulu = pytz.timezone('UTC')
+    now = datetime.now(tz)
+    if now.dst() == timedelta(0):
+        now = datetime.now(zulu) - timedelta(hours=6)
+        #print('Standard Time')
+
+    else:
+        now = datetime.now(zulu) - timedelta(hours=5)
+        #print('Daylight Savings')   
+        
+    return now 
         
 def calculate_time_delta(id: int, date: datetime) -> float:
         """_summary_
@@ -39,7 +61,7 @@ def calculate_time_delta(id: int, date: datetime) -> float:
             datetimeCheckInObject = datetime.fromisoformat(checkInIsoString)        # Convert the ISO strings to datetime objects
             
         except IndexError:
-            print(f'Employee ID #{id} never clocked in on {dateToCalulate}')
+            #print(f'Employee ID #{id} never clocked in on {dateToCalulate}')
             clockedIn = False
             datetimeCheckInObject = None
 
@@ -51,15 +73,17 @@ def calculate_time_delta(id: int, date: datetime) -> float:
         try:
             checkOutIsoString = finalResult[0][GC.TIMESTAMP_COLUMN_NUMBER]
             datetimeCheckOutObject = datetime.fromisoformat(checkOutIsoString)      # Convert the ISO strings to datetime objects
-            datetimeCheckOutObject = None
             
         except IndexError:
-            print(f'Employee ID #{id} never clocked out on {dateToCalulate}')
+            #print(f'Employee ID #{id} never clocked out on {dateToCalulate}')
             clockedOut = False
+            datetimeCheckOutObject = None
 
         elaspedHours = 0
         if(not clockedIn and not clockedOut):
             elaspedHours = 0.0
+        elif(not clockedIn and clockedOut):
+            elaspedHours = 12.0
         elif(clockedIn and not clockedOut):
             elaspedHours = 12.0
         elif(not clockedIn and not clockedOut):
@@ -71,21 +95,76 @@ def calculate_time_delta(id: int, date: datetime) -> float:
                 elaspedSeconds = timeDeltaObject.seconds
                 elaspedHours = elaspedSeconds / 3600.0
         
-        return elaspedHours
+        return elaspedHours, clockedIn, clockedOut
    
+def job():
+    employeeIDs = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025]
+    
+    dates = []
+    for dayDelta in range(8, 1, -1):
+        dates.append((get_date_time() - timedelta(days=dayDelta)).isoformat(timespec="minutes")[0:10])
+    
+    filename = dates[0] + '_' + dates[6] + '_LaborerTimeReport.csv'
+    with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Employee ID', 'Total Hours', 'Sundday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'CheckIn Comment', 'CheckOut Comment'])
+        for id in employeeIDs:
+            dailyHours = []
+            dailyCheckedIn = []
+            dailyCheckedOut = []
+            for day in dates:
+                dateToCalculate = datetime.fromisoformat(day)
+                hoursWorks, checkedIn, checkedOut = calculate_time_delta(id, dateToCalculate)
+                dailyHours.append(round(hoursWorks, 4))
+                dailyCheckedIn.append(checkedIn)
+                dailyCheckedOut.append(checkedOut)
+            
+            totalHours = sum(dailyHours)
+            inComment = 'Missed: '
+            outComment = 'Missed: '
+            dayOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+            for i in range(7):
+                if dailyCheckedIn[i] == False:
+                    inComment = inComment + dayOfWeek[i] + ' '
+                if dailyCheckedOut[i] == False:
+                    outComment = outComment + dayOfWeek[i] + ' '
+            
+            
+            if totalHours == 0:
+                data = [id, round(totalHours, 2), dailyHours[0], dailyHours[1], dailyHours[2], dailyHours[3], dailyHours[4], dailyHours[5], dailyHours[6], 'Missed: All Days', 'Missed: All Days']
+            else:
+                data = [id, round(totalHours, 2), dailyHours[0], dailyHours[1], dailyHours[2], dailyHours[3], dailyHours[4], dailyHours[5], dailyHours[6], inComment, outComment]
+            
+            writer.writerow(data)
+
+def run_continuously(interval=2*GC.ONE_HOUR):
+    """Continuously run, while executing pending jobs at each
+    elapsed time interval.
+    @return cease_continuous_run: threading. Event which can
+    be set to cease continuous run. Please note that it is
+    *intended behavior that run_continuously() does not run
+    missed jobs*. For example, if you've registered a job that
+    should run every minute and you set a continuous run
+    interval of one hour then your job won't be run 60 times
+    at each interval but only once.
+    """
+    cease_continuous_run = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+    return cease_continuous_run
+
+
+
 if __name__ == "__main__":
-    id = 1001
-    date = datetime.fromisoformat("2023-08-22")
-    print(calculate_time_delta(id, date))
-    
-    date = datetime.fromisoformat("2023-08-23")
-    print(calculate_time_delta(id, date))
-    
-    date = datetime.fromisoformat("2023-08-24")
-    print(calculate_time_delta(id, date))
-    
-    date = datetime.fromisoformat("2023-08-25")
-    print(calculate_time_delta(id, date))
-    
-    date = datetime.fromisoformat("2023-08-26")
-    print(calculate_time_delta(id, date))
+    schedule.every().sunday.at("02:00").do(job)
+    process = run_continuously()
+    while True:
+        pass
